b4938c3ff0fc511021bc4d8a7a1c8e91f1f7223d
Sanlem <vfranchook@gmail.com>
2016-01-30 13:36:35 +0200

    ticket:1 initial data now loads via data migrations

--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from django.core.management import call_command
 from apps.contacts.models import Contacts
 
 
 def contacts_list(request):
-    call_command('loaddata', 'contacts.json', verbosity=0)
     contacts = Contacts.objects.all()[:3]
     return render(request, 'contacts.html', {"contacts": contacts})
