b6163cc28b42db50c64d57f0ca9d373767f59ee5
Sanlem <vfranchook@gmail.com>
2016-01-31 18:28:19 +0200

    ticket:3 endpoint passes tests.

--- apps/requests/views.py
+++ apps/requests/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from apps.requests.models import RequestEntry
+from apps.requests.serializers import RequestEntrySerializer
+from rest_framework import generics
 
 
 def requests_list(request):
     objects = RequestEntry.objects.all()[:10]
     return render(request, "requests.html", {"objects": objects})
+
+
+class RequestEntryListView(generics.ListAPIView):
+    queryset = RequestEntry.objects.all()[:10]
+    serializer_class = RequestEntrySerializer
---
+++ assets/js/requests.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    var getRequests = function(){
+
+    };
+    var defaultTitle = document.title;
+    
+});
