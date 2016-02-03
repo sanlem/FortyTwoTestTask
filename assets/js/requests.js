caff35c66018c55889340c174c9f8e716c41fc2f
Sanlem <vfranchook@gmail.com>
2016-02-03 13:34:56 +0200

    ticket:3 removed js and css from html

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -7,67 +7,67 @@ $(document).ready(function() {
     var newRequestsCounter = 0;
     // remember id of newest request
     var newestId = $('.request:first').attr('id');
+    var apiUrl = $('input#api').val();
     
     function updateRequests() {
-    	var url = "{% url 'requests_api' %}" + '?pk=' + newestId.toString();
-    	$.get(url, function(data, status){
-    	if(data[0]){
+        var url = apiUrl + '?pk=' + newestId.toString();
+        $.get(url, function(data, status){
+        if(data[0]){
             newestId = data[0]['id'];
         };
-        dat
-    	data.reverse().forEach(function(obj, i, data) {
-    		if ($('tr.request').length == 10) {           
-					$('tr.request:last').remove();
-			};
-    		// create new row
-    		var row = $('<tr/>', {          
-				'class': 'row request new',
-				'id': obj.id
-			});
-    		// add method col
-    		$('<td/>', {
-				'class': 'col-xs-1',
-				text: obj.method
-			}).appendTo(row);
-    		// add path col
-    		$('<td/>', {
-				'class': 'col-xs-7',
-				text: obj.absolute_path     
-			}).appendTo(row);
-    		// add is_ajax col
-    		$('<td/>', {
-				'class': 'col-xs-1',
-				text: obj.is_ajax.toString().capitalize()
-			}).appendTo(row);
-    		// add timestamp
-    		$('<td/>', {
-    			'class': 'col-xs-3',
-    			text: Date(obj.timestamp).toString()
-    		}).appendTo(row);
-    		// increment counter
-    		newRequestsCounter ++;
-    		document.title = '(' + newRequestsCounter.toString() + ') ' + defaultTitle;
-    		$('.header').after(row).fadeIn();
-    	});
+        data.reverse().forEach(function(obj, i, data) {
+            if ($('tr.request').length == 10) {           
+                    $('tr.request:last').remove();
+            };
+            // create new row
+            var row = $('<tr/>', {          
+                'class': 'row request new',
+                'id': obj.id
+            });
+            // add method col
+            $('<td/>', {
+                'class': 'col-xs-1',
+                text: obj.method
+            }).appendTo(row);
+            // add path col
+            $('<td/>', {
+                'class': 'col-xs-7',
+                text: obj.absolute_path     
+            }).appendTo(row);
+            // add is_ajax col
+            $('<td/>', {
+                'class': 'col-xs-1',
+                text: obj.is_ajax.toString().capitalize()
+            }).appendTo(row);
+            // add timestamp
+            $('<td/>', {
+                'class': 'col-xs-3',
+                text: Date(obj.timestamp).toString()
+            }).appendTo(row);
+            // increment counter
+            newRequestsCounter ++;
+            document.title = '(' + newRequestsCounter.toString() + ') ' + defaultTitle;
+            $('.header').after(row).fadeIn();
+        });
     });
 };
     setInterval(function(){ 
        updateRequests();
-	}, 1000);
+    }, 1000);
 
-	function setAsViewed() {
-    	$('tr.request.new').removeClass('new');
-    	document.title = defaultTitle;
-    	newRequestsCounter = 0;
+    function setAsViewed() {
+        $('tr.request.new').removeClass('new');
+        document.title = defaultTitle;
+        newRequestsCounter = 0;
     };
 
     $(window).focus(function(){
-    	setTimeout(function(){
-    		setAsViewed();
-    	}, 500);
+        setTimeout(function(){
+            setAsViewed();
+        }, 500);
     });
 
     $(document).bind('mousemove keydown scroll', function(){
-    	setAsViewed();
+        setAsViewed();
     });
 });
