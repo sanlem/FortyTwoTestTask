076f01d831416c0c4e0028f05dae0b92a0737255
Sanlem <vfranchook@gmail.com>
2016-02-03 19:37:52 +0200

    ticket:5 added tests for edit view

--- apps/contacts/tests.py
+++ apps/contacts/tests.py
@@ -77,3 +77,62 @@ class TestContactsModel(TestCase):
                             other_contacts="blabla")
 
         self.assertEqual(str(contacts), "Myname Mylastname's contacts")
+
+
+class TestContactsEditView(TestCase):
+    
+    def setUp(self):
+        # create contacts object
+        self.contacts, created = \
+            Contacts.objects.get_or_create(name="Myname",
+                                           lastname="Mylastname",
+                                           email="myemail",
+                                           date_of_birth=datetime.today(),
+                                           jabber_id="myjabber",
+                                           skype_login="myskype",
+                                           bio="bio\r\n!",
+                                           other_contacts="bla\r\nbla")
+
+        # remember client
+        self.client = Client()
+
+        # remember url
+        self.url = reverse("contacts_edit")
+
+    def test_contacts_edit_view(self):
+        """ test contacts' edition """
+        response = self.client.get(self.url)
+        # ensure we have form on the page
+        self.assertIn('<form', response.content)
+        # ensure form has right method and url
+        self.assertIn("action='" + reverse('contacts_edit') + "'", response.content)
+        self.assertIn("method='POST'", response.content)
+        # ensure there are control elements with Bootstrap classes
+        self.assertIn('form-control', response.content)
+        
+        response = self.client.post(self.url, { name: 'Pavlo'})
+        self.assertEqual(response.status_code, 200)
+        # ensure the name is changed
+        self.assertEqual(Contacts.objects.last().name, 'Pavlo')
+
+        contacts = Contacts(name="Myname",
+                            lastname="Mylastname",
+                            email="myemail",
+                            date_of_birth=datetime.today(),
+                            jabber_id="myjabber",
+                            skype_login="myskype",
+                            bio="bio!",
+                            other_contacts="blabla")
+
+        # ensure view edits the newest object
+        response = self.client.post(self.url, { name: 'Vitaliy'})
+        self.assertEqual(Contacts.objects.get(2).name, 'Vitaliy')
+        self.assertEqual(Contacts.objects.get(1).name, 'Pavlo')
+
+        Contacts.objects.all().delete()
+        # should render 'Can't edit. No contacts exist.'
+        response = self.client.get(self.url)
+        self.assertIn("Can't edit. No contacts exist.", response.content)
+        # should return 400 status code
+        response = self.client.post(self.url, { name: 'Vitaliy'})
+        self.assertEqual(response.status_code, 400)
